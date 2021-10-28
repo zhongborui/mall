360f14f476553b57364f8381deaa7d58f93e6d1c
zhongborui <zhongborui77@163.com>
2021-10-28 20:31:02 +0800

    修改项目名

---
+++ mall-core/mall-product/src/main/java/com/arui/mall/product/controller/BaseCategory1Controller.java
@@ -0,0 +1,20 @@
+package com.arui.mall.product.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 一级分类表 前端控制器
+ * </p>
+ *
+ * @author ...
+ * @since 2021-10-28
+ */
+@RestController
+@RequestMapping("/base-category1")
+public class BaseCategory1Controller {
+
+}
+
---
+++ mall-core/mall-product/src/main/java/com/arui/mall/product/service/impl/PlatformPropertyNameServiceImpl.java
@@ -0,0 +1,34 @@
+package com.arui.mall.product.service.impl;
+
+import com.arui.mall.model.pojo.entity.PlatformPropertyName;
+import com.arui.mall.model.pojo.vo.PlatformPropertyVO;
+import com.arui.mall.product.mapper.PlatformPropertyNameMapper;
+import com.arui.mall.product.service.PlatformPropertyNameService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 属性表 服务实现类
+ * </p>
+ *
+ * @author ...
+ * @since 2021-10-28
+ */
+@Service
+public class PlatformPropertyNameServiceImpl extends ServiceImpl<PlatformPropertyNameMapper, PlatformPropertyName> implements PlatformPropertyNameService {
+
+    /**
+     * 根据三级id查询商品平台属性
+     * @param category1ID
+     * @param category2ID
+     * @param category3ID
+     * @return
+     */
+    @Override
+    public List<PlatformPropertyVO> getPlatformPropertyByCategoryId(Long category1ID, Long category2ID, Long category3ID) {
+        return baseMapper.getPlatformPropertyByCategoryId(category1ID, category2ID, category3ID);
+    }
+}

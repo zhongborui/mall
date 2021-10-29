0cd9868e084aa1aaaf5a4f58efe1550e605821fa
zhongborui <zhongborui77@163.com>
2021-10-29 22:55:39 +0800

    实现图片上传到fastdfs文件系统

--- mall-core/mall-product/src/main/java/com/arui/mall/product/controller/admin/AdminBaseBrandController.java
+++ mall-core/mall-product/src/main/java/com/arui/mall/product/controller/admin/AdminBaseBrandController.java
@@ -8,9 +8,16 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.csource.common.MyException;
+import org.csource.fastdfs.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 /**
  * <p>
@@ -27,6 +34,9 @@ public class AdminBaseBrandController {
 
     @Resource
     private BaseBrandService baseBrandService;
+
+    @Value(value = "${fastdfs.prefix}")
+    private String prefix;
 
     /**
      * //http://127.0.0.1/product/brand/queryBrandByPage/1/10
@@ -78,6 +88,37 @@ public class AdminBaseBrandController {
             @PathVariable Long id){
         baseBrandService.removeById(id);
         return R.ok();
+    }
+
+
+    @ApiOperation(value = "上传文件到fastDFS")
+    @PostMapping("/brand/fileUpload")
+    public R fileUpload(
+            @ApiParam(value = "上传的文件")
+            MultipartFile file) throws IOException, MyException {
+        // 获取fastDFS服务器信息文件
+        String configFilePath = this.getClass().getResource("/tracker.conf").getFile();
+
+        // 初始化
+        ClientGlobal.init(configFilePath);
+
+        // 创建trackerClient客户端
+        TrackerClient trackerClient = new TrackerClient();
+
+        // 获取tracker 连接
+        TrackerServer connection = trackerClient.getConnection();
+
+        // 创建StroageClient1
+        StorageClient1 storageClient1 = new StorageClient1(connection, null);
+
+        // 文件原始名称
+        String originalFilename = file.getOriginalFilename();
+        String extensionName = FilenameUtils.getExtension(originalFilename);
+
+        // 实现上传
+        String path = storageClient1.upload_appender_file1(file.getBytes(), extensionName, null);
+        String imageUrl = prefix + path;
+        return R.ok(imageUrl);
     }
 }
 

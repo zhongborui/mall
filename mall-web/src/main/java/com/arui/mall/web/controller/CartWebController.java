b05d93a630177280f36102607e8d96c92a9fa0ac
zhongborui <zhongborui77@163.com>
2021-11-11 20:44:51 +0800

    实现添加购物车时页面显示基本信息

--- mall-web/src/main/java/com/arui/mall/web/controller/CartWebController.java
+++ mall-web/src/main/java/com/arui/mall/web/controller/CartWebController.java
@@ -1,8 +1,13 @@
 package com.arui.mall.web.controller;
 
 import com.arui.mall.common.result.R;
+import com.arui.mall.feign.client.ProductFeignClient;
+import com.arui.mall.model.pojo.vo.SkuInfoVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author ...
@@ -11,8 +16,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CartWebController {
 
+    @Resource
+    private ProductFeignClient productFeignClient;
+
+    /**
+     * 添加购物车，页面显示基本信息
+     * @param request
+     * @return
+     */
     @GetMapping("addCart.html")
-    public String addCart(){
+    public String addCart(HttpServletRequest request){
+        String skuId = request.getParameter("skuId");
+        String skuNum = request.getParameter("skuNum");
+        SkuInfoVO skuInfoVO = productFeignClient.getSkuDetailById(Long.parseLong(skuId));
+        request.setAttribute("skuInfo", skuInfoVO);
+        request.setAttribute("skuNum", skuNum);
         return "cart/addCart";
     }
 }
